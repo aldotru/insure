4065def7d0892f0f29b9d9af9279834b96170059
Aldo Trujillo <aldo.trujillo96@gmail.com>
2020-11-22 12:16:17 -0600

    First mobile navigation functionallity - testing

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const hamburguerMenu = document.querySelector(".hamburguer__icon");
+const hamburguerNav = document.querySelector(".mobile__nav");
+
+hamburguerMenu.addEventListener('click', function() {
+
+    hamburguerNav.classList.toggle('open');
+
+});
