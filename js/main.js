e3a81fda43680f244dc93732fdb3e196434bf4d8
Aldo Trujillo <aldo.trujillo96@gmail.com>
2020-11-24 09:37:08 -0600

    Fixed mobile navigation functionallity

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const hamburguerMenu = document.querySelector(".hamburguer__icon");
 const hamburguerNav = document.querySelector(".mobile__nav");
 
-hamburguerMenu.addEventListener('click', function() {
+hamburguerMenu.addEventListener("click", function() {
 
-    hamburguerNav.classList.toggle('open');
+    hamburguerNav.classList.toggle("open");
 
 });
